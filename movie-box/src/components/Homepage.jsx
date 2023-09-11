a20fa0075e63a59396f52fb0cddfc43edffdbca1
Joel <ajiolajoel@gmail.com>
2023-09-11 18:35:28 +0100

    update to movie-box

---
+++ movie-box/src/components/Homepage.jsx
@@ -0,0 +1,248 @@
+import React from "react";
+import logo from "../assets/Logo.svg";
+import imdb from "../assets/MV5BMTk3ODA4Mjc0NF5BMl5BcG5nXkFtZTgwNDc1MzQ2OTE@ 1.svg";
+import pignet from "../assets/PngItem_1381056 1.svg";
+import headingBackground from "../assets/Poster.svg";
+import { AiOutlineSearch } from "react-icons/ai";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { LuPlayCircle } from "react-icons/lu";
+import { BsSuitHeart } from "react-icons/bs";
+import stranger from "../assets/Posterstranger.svg";
+
+const Homepage = () => {
+  const headingBckground = {
+    backgroundImage: `url(${headingBackground})`,
+    height: "100vh",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+  };
+  const strangerThings = {
+    backgroundImage: `url(${stranger})`,
+    backgroundSize: "contain",
+    height: "300px",
+    width: "1500px",
+    backgroundRepeat: "no-repeat",
+  };
+  const Font = {
+    fontFamily: " 'DM Sans', sans-serif",
+  };
+
+  return (
+    <div>
+      <div style={headingBckground}>
+        <div className="flex p-5 justify-around">
+          <img src={logo} alt="logo-hero" />
+          <div className="flex justify-between mt-3">
+            <input
+              type="text"
+              name=""
+              id=""
+              className="bg-transparent border border-white text-white w-[500px] rounded-md h-10 p-3"
+              placeholder="what do you want to watch?"
+              style={Font}
+            />
+            <AiOutlineSearch className="text-white relative top-4 right-5" />
+          </div>
+          <div className="flex mt-3 gap-8">
+            <a href="#" className="text-white no-underline" style={Font}>
+              Sign in
+            </a>
+            <HiMenuAlt4 className=" bg-[#BE123C] text-white rounded-full w-[30px] h-[30px] cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex flex-col px-36 gap-5 py-20">
+          <h3 className="text-white text-5xl font-bold" style={Font}>
+            John Wick 3: <br />
+            Parabellum
+          </h3>
+          <div className="flex gap-4">
+            <div className="flex gap-5">
+              <img src={imdb} alt="imdbtmv" />
+              <p className="text-white" style={Font}>
+                86.0/100
+              </p>
+            </div>
+            <div className="flex gap-5">
+              <img src={pignet} alt="pigneeterre" />
+              <p className="text-white" style={Font}>
+                97%
+              </p>
+            </div>
+          </div>
+          <p className="text-white" style={Font}>
+            John Wick is on the run after killing a member
+            <br />
+            of the international assassins' guild, and with
+            <br />
+            a $14 million price tag on his head, he is the
+            <br />
+            target of hit men and women everywhere.
+          </p>
+          <div className="bg-[#BE123C] rounded-md flex w-fit p-2 gap-3 mt-9">
+            <LuPlayCircle className="mt-[5px] text-white" />
+            <a href="http://" className="text-white" style={Font}>
+              WATCH TRAILER
+            </a>
+          </div>
+        </div>
+        </div>
+     <div className="flex overflow-x-hidden">
+     <div className="flex flex-col">
+          <div style={strangerThings} className=" p-4">
+            <div className="flex flex-col">
+              <div className="flex gap-14">
+                <p
+                  className="bg-slate-300 border rounded-full text-black px-3"
+                  style={Font}
+                >
+                  TV SERIES
+                </p>
+                <BsSuitHeart className="rounded-full text-white bg-slate-500 w-5 h-5" />
+              </div>
+            </div>
+          </div>
+          <p style={Font} className="text-[#9CA3AF] font-bold text-xs">
+                USA, 2016 - Current
+              </p>
+              <p style={Font} className="text-[#111827] text-lg font-bold">
+                Stranger Things
+              </p>
+              <div className="flex gap-4">
+                <div className="flex gap-5">
+                  <img src={imdb} alt="imdbtmv" />
+                  <p className="text-[#111827]" style={Font}>
+                    86.0/100
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <img src={pignet} alt="pigneeterre" />
+                  <p className="text-[#111827]" style={Font}>
+                    97%
+                  </p>
+                </div>
+              </div>
+              <p style={Font} className="text-[#9CA3AF] font-bold">
+                Action, Adventurrrre, Horror
+              </p>
+              </div> 
+              <div className="flex flex-col">
+          <div style={strangerThings} className=" p-4">
+            <div className="flex flex-col">
+              <div className="flex gap-14">
+                <p
+                  className="bg-slate-300 border rounded-full text-black px-3"
+                  style={Font}
+                >
+                  TV SERIES
+                </p>
+                <BsSuitHeart className="rounded-full text-white bg-slate-500 w-5 h-5" />
+              </div>
+            </div>
+          </div>
+          <p style={Font} className="text-[#9CA3AF] font-bold text-xs">
+                USA, 2016 - Current
+              </p>
+              <p style={Font} className="text-[#111827] text-lg font-bold">
+                Stranger Things
+              </p>
+              <div className="flex gap-4">
+                <div className="flex gap-5">
+                  <img src={imdb} alt="imdbtmv" />
+                  <p className="text-[#111827]" style={Font}>
+                    86.0/100
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <img src={pignet} alt="pigneeterre" />
+                  <p className="text-[#111827]" style={Font}>
+                    97%
+                  </p>
+                </div>
+              </div>
+              <p style={Font} className="text-[#9CA3AF] font-bold">
+                Action, Adventurrrre, Horror
+              </p>
+              </div> 
+              <div className="flex flex-col">
+          <div style={strangerThings} className=" p-4">
+            <div className="flex flex-col">
+              <div className="flex gap-14">
+                <p
+                  className="bg-slate-300 border rounded-full text-black px-3"
+                  style={Font}
+                >
+                  TV SERIES
+                </p>
+                <BsSuitHeart className="rounded-full text-white bg-slate-500 w-5 h-5" />
+              </div>
+            </div>
+          </div>
+          <p style={Font} className="text-[#9CA3AF] font-bold text-xs">
+                USA, 2016 - Current
+              </p>
+              <p style={Font} className="text-[#111827] text-lg font-bold">
+                Stranger Things
+              </p>
+              <div className="flex gap-4">
+                <div className="flex gap-5">
+                  <img src={imdb} alt="imdbtmv" />
+                  <p className="text-[#111827]" style={Font}>
+                    86.0/100
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <img src={pignet} alt="pigneeterre" />
+                  <p className="text-[#111827]" style={Font}>
+                    97%
+                  </p>
+                </div>
+              </div>
+              <p style={Font} className="text-[#9CA3AF] font-bold">
+                Action, Adventurrrre, Horror
+              </p>
+              </div> 
+              <div className="flex flex-col">
+          <div style={strangerThings} className=" p-4">
+            <div className="flex flex-col">
+              <div className="flex gap-14">
+                <p
+                  className="bg-slate-300 border rounded-full text-black px-3"
+                  style={Font}
+                >
+                  TV SERIES
+                </p>
+                <BsSuitHeart className="rounded-full text-white bg-slate-500 w-5 h-5" />
+              </div>
+            </div>
+          </div>
+          <p style={Font} className="text-[#9CA3AF] font-bold text-xs">
+                USA, 2016 - Current
+              </p>
+              <p style={Font} className="text-[#111827] text-lg font-bold">
+                Stranger Things
+              </p>
+              <div className="flex gap-4">
+                <div className="flex gap-5">
+                  <img src={imdb} alt="imdbtmv" />
+                  <p className="text-[#111827]" style={Font}>
+                    86.0/100
+                  </p>
+                </div>
+                <div className="flex gap-5">
+                  <img src={pignet} alt="pigneeterre" />
+                  <p className="text-[#111827]" style={Font}>
+                    97%
+                  </p>
+                </div>
+              </div>
+              <p style={Font} className="text-[#9CA3AF] font-bold">
+                Action, Adventurrrre, Horror
+              </p>
+              </div> 
+              {/* end of first row */}
+     </div>
+    </div>
+  );
+};
+
+export default Homepage;
